6dd2ea10240d53255d3e13e65c8e4388a9c7cfca
korenrolle <korenrolle21@gmail.com>
2022-11-15 14:37:54 -0800

    was able to get the getRandom function running

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-console.log('We out here');
-document.querySelector('#spinner').addEventListener('click', spin);
+function getRandom() {
+  document.getElementById('square1').innerHTML = Math.floor(Math.random() * 13);
+  console.log();
+  document.getElementById('square2').innerHTML = Math.floor(Math.random() * 13);
+  console.log();
+  document.getElementById('square3').innerHTML = Math.floor(Math.random() * 13);
+  console.log();
+  // }
+  // function getRandom() {
+  //   document.getElementById('square2').innerHTML = Math.floor(Math.random() * 12);
+  //   console.log();
+  // }
+  // function getRandom() {
+  //   document.getElementById('square3').innerHTML = Math.floor(Math.random() * 12);
+  //   console.log();
+}
+getRandom();
